2f738112c237c2e7b957804d1e983854fe5cf4d0
Troy Dorman <troydorman@gmail.com>
2021-08-25 09:36:50 -0600

    creating tags based on keyup event listener in the text area removing any white space entered, splitting by commas

---
+++ script.js
@@ -0,0 +1,16 @@
+const tagsEl = document.querySelector(".tags");
+const textarea = document.querySelector("textarea");
+
+textarea.focus();
+
+textarea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+});
+
+function createTags(input) {
+  const tags = input
+    .split(",")
+    .filter((tag) => tag.trim() !== "")
+    .map((tag) => tag.trim());
+  console.log(tags);
+}
