aea4e994b986401b5ec3e906cb52815973dc2817
Troy Dorman <troydorman@gmail.com>
2021-08-25 09:41:36 -0600

    for each comma separated value we create a tag element and display it in the tags area

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const tagsEl = document.querySelector(".tags");
-const textarea = document.querySelector("textarea");
+const tagsEl = document.getElementById("tags");
+const textarea = document.getElementById("textarea");
 
 textarea.focus();
 
@@ -12,5 +12,13 @@ function createTags(input) {
     .split(",")
     .filter((tag) => tag.trim() !== "")
     .map((tag) => tag.trim());
-  console.log(tags);
+
+  tagsEl.innerHTML = "";
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement("span");
+    tagEl.classList.add("tag");
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
 }
